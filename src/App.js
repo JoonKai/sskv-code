2462516f8b68e1df46b1522823194949e2de993c
JoonKai <joonseok831@gmail.com>
2022-04-05 23:59:28 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './screens/Home';
+import About from './screens/About';
+import Projects from './screens/Projects';
+import Questions from './screens/Questions';
+import ReactDoc from './screens/ReactDoc';
+import NavBarElements from './components/NavBarElements';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <NavBarElements/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/About" element={<About/>}/>
+        <Route path="/Projects" element={<Projects/>}/>
+        <Route path="/Questions" element={<Questions/>}/>
+        <Route path="/ReactDoc" element={<ReactDoc/>}/>
+      </Routes>
+    </Router>
   );
 }
 
